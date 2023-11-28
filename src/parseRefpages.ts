c468012cf0ffd3bb361798abdf57165f0d5faf17
Zhixing Zhang <me@neoto.xin>
2023-11-28 11:39:44 +0000

    add code highlight

--- src/parseRefpages.ts
+++ src/parseRefpages.ts
@@ -7,7 +7,39 @@ import { frontmatter } from "micromark-extension-frontmatter";
 import { frontmatterFromMarkdown } from "mdast-util-frontmatter";
 import { parse } from "yaml";
 import assert from "assert";
+import { parseMarkdown } from '@nuxtjs/mdc/runtime';
+import type { MDCParseOptions } from '@nuxtjs/mdc/runtime/types/parser';
+import type { Theme } from '@nuxtjs/mdc/runtime/shiki/types';
+import rehypeShiki from '@nuxtjs/mdc/runtime/shiki/index';
+import { useShikiHighlighter } from '@nuxtjs/mdc/runtime/shiki/highlighter'
 
+export const PROSE_TAGS = [
+    'p',
+    'a',
+    'blockquote',
+    'code-inline',
+    'code',
+    'em',
+    'h1',
+    'h2',
+    'h3',
+    'h4',
+    'h5',
+    'h6',
+    'hr',
+    'img',
+    'ul',
+    'ol',
+    'li',
+    'strong',
+    'table',
+    'thead',
+    'tbody',
+    'td',
+    'th',
+    'tr'
+  ]
+  
 async function main() {
     const metadata: {
         id: string,
@@ -37,8 +69,41 @@ async function main() {
             type: yaml.type,
         })
 
-          
-        const results = await markdownTransformer.parse!(id, file, {});
+        const shikiTheme = {
+            theme: {
+                light: 'material-theme-lighter',
+                default: 'material-theme',
+                dark: 'material-theme-palenight'
+              },
+              preload: [ 'rust', 'c' ],
+        }
+        const parsed = await parseMarkdown(file as string, <MDCParseOptions> {
+            highlight: {
+                ...shikiTheme,
+                highlighter: async (code: string, lang: string, theme: Theme, highlights) => {
+                    const shikiHighlighter = useShikiHighlighter(shikiTheme)
+                    return await shikiHighlighter.getHighlightedAST(code as string, lang as any, theme as Theme, { highlights })
+                  }
+              },
+              remark: { plugins: {} },
+              rehype: { options: { handlers: {} }, plugins: {
+                highlight: {
+                    instance: rehypeShiki,
+                }
+              } },
+              toc: undefined
+          })
+      
+          const results = {
+            ...parsed.data,
+            excerpt: parsed.excerpt,
+            body: {
+              ...parsed.body,
+              toc: parsed.toc
+            },
+            _type: 'markdown',
+            _id: id
+          }
         await writeFile(`./dist/man/${id}.json`, JSON.stringify(results));
     }
     await writeFile('./dist/man/index.json', JSON.stringify(metadata));
